34593d202f2b36071ea0ae27b037242fa502aabf
jpmorgany <jpmorgany@qrypty.com>
2026-07-16 10:06:38 +0200

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,45 +3,11 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://www.v2nodes.com/subscriptions/country/all/?key=2BA5A971FC9B206",
-    "https://raw.githubusercontent.com/sakha1370/OpenRay/refs/heads/main/output_iran/iran_top100_checked.txt",
-    "https://raw.githubusercontent.com/SoliSpirit/v2ray-configs/refs/heads/main/Countries/Iran.txt",
-    "https://raw.githubusercontent.com/ThomasJasperthecat/sub/main/sublist1.txt",
-    "https://raw.githubusercontent.com/hamedp-71/For_All_Net/refs/heads/main/hp.txt",
-    "https://raw.githubusercontent.com/hamedp-71/Sub_Checker_Creator/refs/heads/main/final.txt",
+
     "https://raw.githubusercontent.com/masir-sefid/Sub/main/Telegram-Channel-@Masir_Sefid.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_SS%2BAll_RUS.txt",
     "https://etoneya.su/1",
-    "https://raw.githubusercontent.com/flaafix/AetrisVPN/refs/heads/main/AetrisVPN.txt",
-    "https://raw.githubusercontent.com/flaafix/AetrisVPN-black-list/refs/heads/main/configs.txt",
-    "https://raw.githubusercontent.com/flaafix/AetrisVPN-white-list-lite/refs/heads/main/AetrisVPN.txt",
-    #"https://raw.githubusercontent.com/whoahaow/rjsxrd/refs/heads/main/githubmirror/default/all.txt",
-    "https://sub.proxygo.org/v2ray.php?key=7538c143e413c37e07d54038e95bd9f9",
-    "https://raw.githubusercontent.com/roosterkid/openproxylist/refs/heads/main/V2RAY_BASE64.txt",
-    "https://mifa.world/ss",
-    "https://mifa.world/trojan",
-    "https://mifa.world/hysteria",
-    "https://mifa.world/vmess",
-    "https://mifa.world/vless",
-    "https://mifa.world/other",
-    "https://raw.githubusercontent.com/Delta-Kronecker/V2ray-Config/refs/heads/main/config/all_configs.txt",
-    "https://freeproxydb.com/api/proxy/subscribe?country=&protocol=hysteria2&anonymity=&speed=0,60&https=0&count=500&subscribe_format=v2ray",
-    "https://freeproxydb.com/api/proxy/subscribe?country=&protocol=vmess&anonymity=&speed=0,60&https=0&count=500&subscribe_format=v2ray",
-    "https://freeproxydb.com/api/proxy/subscribe?country=&protocol=vless&anonymity=&speed=0,60&https=0&count=500&subscribe_format=v2ray",
-    "https://freeproxydb.com/api/proxy/subscribe?country=&protocol=trojan&anonymity=&speed=0,60&https=0&count=500&subscribe_format=v2ray",
-    "https://freeproxydb.com/api/proxy/subscribe?country=&protocol=ss&anonymity=&speed=0,60&https=0&count=500&subscribe_format=v2ray",
-    "https://freeproxydb.com/api/proxy/subscribe?country=&protocol=ssr&anonymity=&speed=0,60&https=0&count=500&subscribe_format=v2ray",
-    "https://raw.githubusercontent.com/sakha1370/OpenRay/refs/heads/main/output/all_valid_proxies.txt",
-    "https://raw.githubusercontent.com/LalatinaHub/Mineral/refs/heads/master/result/nodes",
-    "https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/vless.txt",
-    "https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/vmess.txt",
-    "https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/trojan.txt",
-    "https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/ss.txt",
-    "https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/other.txt",
-    "https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/subscriptions/v2ray/all_sub.txt",
-    "https://sub.shadowproxy66.workers.dev/sub/104ea085-c4e8-498a-9988-f2d6acf2e070#ShadowProxy66(4)",
-    "https://cdn.jsdelivr.net/gh/0xRadikal/Free-v2ray-Configs@main/all/configs_base64.txt",
-    "https://raw.githubusercontent.com/aristapanell-cell/AriataPanel/refs/heads/main/config.yaml/combined/ALL/ALL.yaml",
+
 
 ]
 
@@ -88,7 +54,7 @@ SINGBOX_TESTER_TIMEOUT_SECONDS = 10
 # List of URLs to test sing-box configs against.
 # The tester will try each URL in order until one succeeds.
 SINGBOX_TESTER_URLS = [
-    'http://www.youtube.com/generate_204'
+    'https://www.youtube.com/generate_204'
     #'https://www.gstatic.com/generate_204'
 ]
 
@@ -110,7 +76,7 @@ XRAY_TESTER_TIMEOUT_SECONDS = 10
 # List of URLs to test Xray configs against.
 # The tester will try each URL in order until one succeeds.
 XRAY_TESTER_URLS = [
-    'http://www.youtube.com/generate_204'
+    'https://www.youtube.com/generate_204'
     #'https://www.gstatic.com/generate_204'
 ]
 
